79dd68e3562b17e5c0f00bd7ecb3ca09de6aa423
jongim <whddladl91@hanmail.net>
2015-02-08 00:03:05 +0900

    test

--- app.js
+++ app.js
@@ -1,39 +1,115 @@
-var sys = require('sys'),
-    http = require('http'),
-    express = require('express'),
-    app = express(),
-    server = http.createServer(app),
-    io = require('socket.io').listen(server);
+var express = require('express')
+    , routes = require('./routes')
+    , user = require('./routes/users')
+    , http = require('http')
+    , engine = require('ejs-locals')
+    , fs = require('fs')
+    , util = require('util')
+    , url = require('url')
+    , crypto = require('crypto')
+    , path = require('path');
 
-server.listen(13000);
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
 
-app.use(express.static(__dirname + '/public'));
+var app = express();
+var favicon = require('serve-favicon');
+var logger = require('express-logger');
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
+//Hash Key Generator for Password
+var myHash = function myHash(key){
+    var hash = crypto.createHash('sha1');
+    hash.update(key);
+    return hash.digest('hex');
+}
+
+//Create Session
+var createSession = function createSession(){
+    return function(req, res, next){
+        if(!req.session.login){
+            req.session.login = 'logout';
+        }
+        next();
+    };
+};
+
+//all environment
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger({path: "/path/to/logfile.txt"}));
+//app.use(express. body-parser({limit:'800mb', uploadDir:__dirname + '/public/se/uploadTmp'}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.method-override());
+app.use(require('express-method-override')('method_override_param_name'));
+//app.use(express.cookie-parser());
+app.use(cookieParser());
+/*app.use(express.session({ secret: 'keyboard cat', cookie: { maxAge: 600000 }}));
+app.use(createSession());*/
+app.use(session({secret:'secret key'}));
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('ejs',engine);
+
+//main (page:0)
+app.get('/',routes.index);
+
+//Members (page:1)
+app.get('/MEMBERDB', routes.member_db);
+
+//Data View (page:2)
+app.get('/DATAVIEW', routes.dataview);
+
+//Login Button
+app.post('/LOGIN', function(req,res,next) {
+    req.username = req.body.username;
+    req.password = myHash(req.body.password);
+    next();
+},routes.login_post);
+
+//Login Button (Enabled after loggin in)
+app.get('/LOGOUT', routes.logout);
+
+//Sign uo Button
+app.get('/SIGN_UP', routes.sign_up);
+app.post('/SIGN_UP', function(req,res,next) {
+    if(req.body.password == req.body.confirm_password) {
+        req.username = req.body.username;
+        req.password = myHash(req.body.password);
+        req.email = req.body.email;
+        next();
+    }
+    else
+    {
+        res.redirect('/');
+    };
+},routes.sign_up_post);
+
+//Check User Name Button (Display in sign up page)
+app.get('/CHECKUSERNAME',routes.checkusername);
+
+//Naver Smarteditor Image Upload
+app.post('/UPLOAD',function(req,res) {
+    var str = req.header('User-Agent');
+    var os = str.search("Win");
+    var fileName = req.files.file.path;
+    if(os == -1) {
+        fileName = fileName.split('/')[fileName.split('/').length-1];
+    }
+    else {
+        fileName = fileName.split('\\')[fileName.split('\\').length-1];
+    }
+    res.writeHeader(200,{'Content-Type':'text/plain'});
+    res.write('&bNewLine=true');
+    res.write('&sFileName=' + fileName);
+    res.write('&sFileURL=/se/uploadTmp/' + fileName);
+    res.end();
 });
 
-io.sockets.on('connection', function (socket) {
-    // new client is here!
-    console.log('Connection');
+//Naver SmartEditor Content Submit
+app.post('/SUBMIT', routes.insertData);
 
-    socket.on('btn', function(data) {
-        console.log("btn : " + data);
-        io.sockets.emit('btn', data);
-
-    });
-
-    socket.on('pad', function(data, flag) {
-        console.log("pad : " + data + flag);
-        io.sockets.emit('pad', data, flag);
-
-    });
-
-    socket.on('UUID', function(data) {
-       console.log("UUID : " + data);
-    });
-
-    socket.on('disconnection', function() {
-        console.log('disconnection');
-    })
-});
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,177 @@
-var express = require('express');
-var router = express.Router();
+//DB Connection
+var fs = require('fs');
+var mongoose = require('mongoose');
+var url = require('url');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+//connects to MongoDB
+mongoose.connect('mongodb://localhost/membership');
+
+//get the connection from mongoose
+var db = mongoose.connection;
+
+//creates DB schma for MongoDB
+var memberSchema = mongoose.Schema({
+    username: 'string',
+    password: 'string',
+    email: 'string'
+});
+var dataSchema = mongoose.Schema({
+    title: 'string',
+    content: 'string'
 });
 
-module.exports = router;
+//compiles our schema into model
+var Member = mongoose.model('Member', memberSchema);
+var Data = mongoose.model('Data', dataSchema);
+
+//route functions
+// Main (page: 0)
+exports.index = function(req, res) {
+    res.status(200);
+    res.render('index', {
+        title: 'OldGame',
+        page: 0,
+        url: req.url,
+        login: req.session.login,
+        username: req.session.username
+    });
+};
+
+exports.member_db = function(req, res) {
+    var uri = url.parse(req.url,true).query;
+    if(uri.cmd == "del"){
+        Member.remove({_id: uri.id}, function(err,result) {
+            res.status(300);
+            res.redirect('/MEMBERDB');
+        });
+    }
+    else{
+        res.status(200);
+        Member.count({}, function(err, count){
+            Member.find({}, function(err,result){
+                res.render('memberdb', {
+                    title: 'Member DB',
+                    page: 1,
+                    url: req.url,
+                    database: 'local',
+                    collectionName: 'members',
+                    documentCount: count,
+                    myMember: result,
+                    login: req.session.login,
+                    username: req.session.username
+                });
+            });
+        });
+    }
+};
+
+exports.dataview = function(req, res) {
+    Data.count({}, function(err, count) {
+        Data.find({}, function(err,result) {
+            for(var i = 0; i < count; i++) {
+                unblockTag(result[i].content);
+            };
+            res.status(200);
+            res.render('dataview', {
+                title: 'Data View',
+                page: 2,
+                documentCount: count,
+                myData: result,
+                url: req.url,
+                login: req.session.login,
+                username: req.session.username
+            });
+        });
+    });
+};
+
+exports.login_post = function(req, res) {
+    res.status(200);
+    Member.findOne({ username: req.username, password: req.password }, function (err, member) {
+        if(member != null) {
+            req.session.login = 'login';
+            req.session.username = req.username;
+        };
+        res.status(200);
+        res.redirect(url.parse(req.url,true).query.url);
+    });
+};
+
+exports.logout = function(req, res) {
+    req.session.login = 'logout';
+
+    res.status(200);
+    res.redirect(url.parse(req.url,true).query.url);
+};
+
+exports.sign_up = function(req, res) {
+    res.status(200);
+    res.render('sign_up', {
+        title: 'Sign up',
+        url: req.url,
+        page:5,
+        login: req.session.login,
+        username: req.session.username,
+        existingUsername: 'null'
+    });
+};
+
+exports.checkusername = function(req, res) {
+    var uri = url.parse(req.url,true);
+    Member.findOne({ username: uri.query.id }, function (err, member) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        if(member != null) {
+            res.end('true');
+        }
+        else {
+            res.end('false');
+        }
+    });
+}
+
+exports.sign_up_post = function(req, res) {
+    res.status(200);
+
+    var curUsername = req.username;
+    if(curUsername == "") {
+        res.redirect('/');
+    }
+    else {
+        Member.findOne({ username: curUsername }, function (err, member) {
+            if (err) return handleError(err);
+
+            if(member == null) {
+                var myMember = new Member({ username: curUsername, password: req.password, email: req.email });
+                myMember.save(function (err, data) {
+                    if (err) {// TODO handle the error
+                        console.log("error");
+                    }
+                    console.log('member is inserted');
+                });
+                res.redirect('/MEMBERDB');
+            }
+            else {
+                res.redirect('/');
+            }
+        });
+    }
+};
+
+exports.insertData = function(req, res) {
+    if(req.session.login=='login'){
+        var myData = new Data({
+            title: req.body.title,
+            content: req.body.ir1});
+        myData.save(function (err, data) {
+            if (err) {
+                console.log("error");
+            }
+            console.log('message is inserted');
+        });
+    }
+
+    res.status(200);
+    res.redirect(url.parse(req.url,true).query.url);
+};
+
+
