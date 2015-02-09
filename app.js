72d8a98952ca4c0386d33f8cf7c449a67b4e2f9e
jongim <whddladl91@hanmail.net>
2015-02-10 03:36:01 +0900

    웹 2차 수정 (로그인/ 로그아웃, 회원가입)

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
-var express = require('express')
-    , routes = require('./routes')
-    , user = require('./routes/users')
-    , http = require('http')
-    , engine = require('ejs-locals')
-    , fs = require('fs')
-    , util = require('util')
-    , url = require('url')
-    , crypto = require('crypto')
-    , path = require('path');
-
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('cookie-session');
-
+//http 서버
+var http = require('http');
+var express = require('express');
+var routes = require('./routes');
 var app = express();
-var favicon = require('serve-favicon');
-var logger = require('express-logger');
+var server = http.createServer(app);
+server.listen(3000);
 
-//Hash Key Generator for Password
-var myHash = function myHash(key){
+//웹페이지 구성
+var engine = require('ejs-locals');
+var path = require('path');
+var bodyParser = require('body-parser');
+var session = require('cookie-session');
+var favicon = require('serve-favicon');
+
+//비밀번호 암호화
+var crypto = require('crypto');
+var myHash = function myHash(key) {
     var hash = crypto.createHash('sha1');
     hash.update(key);
     return hash.digest('hex');
 }
 
 //Create Session
-var createSession = function createSession(){
-    return function(req, res, next){
-        if(!req.session.login){
+var createSession = function createSession() {
+    return function (req, res, next) {
+        if (!req.session.login) {
             req.session.login = 'logout';
         }
         next();
@@ -39,78 +36,38 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger({path: "/path/to/logfile.txt"}));
-//app.use(express. body-parser({limit:'800mb', uploadDir:__dirname + '/public/se/uploadTmp'}))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(express.method-override());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(require('express-method-override')('method_override_param_name'));
-//app.use(express.cookie-parser());
-app.use(cookieParser());
-/*app.use(express.session({ secret: 'keyboard cat', cookie: { maxAge: 600000 }}));
-app.use(createSession());*/
-app.use(session({secret:'secret key'}));
-//app.use(app.router);
+app.use(session({secret: 'secret key'}));
 app.use(express.static(path.join(__dirname, '/views')));
 
-app.engine('ejs',engine);
+app.engine('ejs', engine);
 
-//main (page:0)
-app.get('/',routes.index);
+app.get('/', routes.index);
 
-//Members (page:1)
-app.get('/MEMBERDB', routes.member_db);
+app.get('/SIGN_UP', routes.sign_up);
 
-//Data View (page:2)
-app.get('/DATAVIEW', routes.dataview);
+app.get('/CHECKUSERNAME', routes.checkUserName);
 
-//Login Button
-app.post('/LOGIN', function(req,res,next) {
+app.post('/SIGN_UP', function (req, res, next) {
+    if (req.body.password == req.body.confirm_password) {
+        req.username = req.body.username;
+        req.password = myHash(req.body.password);
+        next();
+    }
+    else {
+        res.redirect('/SIGN_UP');
+    }
+}, routes.sign_up_post);
+
+app.post('/LOGIN', function (req, res, next) {
     req.username = req.body.username;
     req.password = myHash(req.body.password);
     next();
-},routes.login_post);
+}, routes.login_post);
 
-//Logout Button (Enabled after loggin in)
 app.get('/LOGOUT', routes.logout);
 
-//Sign up Button
-app.get('/SIGN_UP', routes.sign_up);
-app.post('/SIGN_UP', function(req,res,next) {
-    if(req.body.password == req.body.confirm_password) {
-        req.username = req.body.username;
-        req.password = myHash(req.body.password);
-        req.email = req.body.email;
-        next();
-    }
-    else
-    {
-        res.redirect('/');
-    };
-},routes.sign_up_post);
+app.get('/MEMBERDB', routes.member_db);
 
-//Check User Name Button (Display in sign up page)
-app.get('/CHECKUSERNAME',routes.checkusername);
-
-//Naver Smarteditor Image Upload
-/*app.post('/UPLOAD',function(req,res) {
-    var str = req.header('User-Agent');
-    var os = str.search("Win");
-    var fileName = req.files.file.path;
-    if(os == -1) {
-        fileName = fileName.split('/')[fileName.split('/').length-1];
-    }
-    else {
-        fileName = fileName.split('\\')[fileName.split('\\').length-1];
-    }
-    res.writeHeader(200,{'Content-Type':'text/plain'});
-    res.write('&bNewLine=true');
-    res.write('&sFileName=' + fileName);
-    res.write('&sFileURL=/se/uploadTmp/' + fileName);
-    res.end();
-});*/
-
-//Naver SmartEditor Content Submit
-app.post('/SUBMIT', routes.insertData);
-
-module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 //DB Connection
-var fs = require('fs');
 var mongoose = require('mongoose');
 var url = require('url');
 
 //connects to MongoDB
-mongoose.connect('mongodb://localhost/membership');
+mongoose.connect('mongodb://210.118.74.103:27017/membership');
 
 //get the connection from mongoose
 var db = mongoose.connection;
@@ -12,21 +11,19 @@ var db = mongoose.connection;
 //creates DB schma for MongoDB
 var memberSchema = mongoose.Schema({
     username: 'string',
-    password: 'string',
-    email: 'string'
-});
-var dataSchema = mongoose.Schema({
-    title: 'string',
-    content: 'string'
+    password: 'string'
 });
 
 //compiles our schema into model
 var Member = mongoose.model('Member', memberSchema);
-var Data = mongoose.model('Data', dataSchema);
 
 //route functions
 // Main (page: 0)
-exports.index = function(req, res) {
+exports.index = function (req, res) {
+    if (req.session.login === 'login') {
+        res.redirect('http://naver.com');
+        return;
+    }
     res.status(200);
     res.render('index', {
         title: 'OldGame',
@@ -37,18 +34,26 @@ exports.index = function(req, res) {
     });
 };
 
-exports.member_db = function(req, res) {
-    var uri = url.parse(req.url,true).query;
-    if(uri.cmd == "del"){
-        Member.remove({_id: uri.id}, function(err,result) {
+exports.member_db = function (req, res) {
+
+    if (req.session.login !== 'login') {
+        res.redirect('/');
+        //res.status(401).send('Not Login').end();
+        return;
+    }
+
+
+    var uri = url.parse(req.url, true).query;
+    if (uri.cmd == "del") {
+        Member.remove({_id: uri.id}, function (err, result) {
             res.status(300);
             res.redirect('/MEMBERDB');
         });
     }
-    else{
+    else {
         res.status(200);
-        Member.count({}, function(err, count){
-            Member.find({}, function(err,result){
+        Member.count({}, function (err, count) {
+            Member.find({}, function (err, result) {
                 res.render('memberdb', {
                     title: 'Member DB',
                     page: 1,
@@ -65,34 +70,14 @@ exports.member_db = function(req, res) {
     }
 };
 
-exports.dataview = function(req, res) {
-    Data.count({}, function(err, count) {
-        Data.find({}, function(err,result) {
-            for(var i = 0; i < count; i++) {
-                unblockTag(result[i].content);
-            };
-            res.status(200);
-            res.render('dataview', {
-                title: 'Data View',
-                page: 2,
-                documentCount: count,
-                myData: result,
-                url: req.url,
-                login: req.session.login,
-                username: req.session.username
-            });
-        });
-    });
-};
-
-exports.login_post = function(req, res) {
+exports.login_post = function (req, res) {
     res.status(200);
-    Member.findOne({ username: req.username, password: req.password }, function (err, member) {
-        if(member != null) {
+    Member.findOne({username: req.username, password: req.password}, function (err, member) {
+        if (member != null) {
             req.session.login = 'login';
             req.session.username = req.username;
             res.status(200);
-            res.redirect(url.parse(req.url,true).query.url + "MEMBERDB");
+            res.redirect(url.parse(req.url, true).query.url + "MEMBERDB");
         }
         else {
             res.status(200);
@@ -102,20 +87,20 @@ exports.login_post = function(req, res) {
     });
 };
 
-exports.logout = function(req, res) {
+exports.logout = function (req, res) {
     req.session.login = 'logout';
 
     res.status(200);
     res.redirect('/');
 };
 
-exports.sign_up = function(req, res) {
+exports.sign_up = function (req, res) {
     res.status(200);
-    //res.redirect(url.parse(req.url,true).query.url + "SIGN_UP");
+
     res.render('sign_up', {
         title: 'Sign up',
         url: req.url,
-        page:5,
+        page: 5,
         login: req.session.login,
         username: req.session.username,
         existingUsername: 'null'
@@ -123,11 +108,11 @@ exports.sign_up = function(req, res) {
 
 };
 
-exports.checkusername = function(req, res) {
-    var uri = url.parse(req.url,true);
-    Member.findOne({ username: uri.query.id }, function (err, member) {
+exports.checkUserName = function (req, res) {
+    var uri = url.parse(req.url, true);
+    Member.findOne({username: uri.query.id}, function (err, member) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        if(member != null) {
+        if (member != null) {
             res.end('true');
         }
         else {
@@ -136,19 +121,18 @@ exports.checkusername = function(req, res) {
     });
 }
 
-exports.sign_up_post = function(req, res) {
+exports.sign_up_post = function (req, res) {
     res.status(200);
 
     var curUsername = req.username;
-    if(curUsername == "") {
-        res.redirect('/');
-    }
-    else {
-        Member.findOne({ username: curUsername }, function (err, member) {
+    if (curUsername == "") {
+        res.redirect('/SIGN_UP');
+    } else {
+        Member.findOne({username: curUsername}, function (err, member) {
             if (err) return handleError(err);
 
-            if(member == null) {
-                var myMember = new Member({ username: curUsername, password: req.password, email: req.email });
+            if (member == null) {
+                var myMember = new Member({username: curUsername, password: req.password});
                 myMember.save(function (err, data) {
                     if (err) {// TODO handle the error
                         console.log("error");
@@ -163,22 +147,4 @@ exports.sign_up_post = function(req, res) {
         });
     }
 };
-
-exports.insertData = function(req, res) {
-    if(req.session.login=='login'){
-        var myData = new Data({
-            title: req.body.title,
-            content: req.body.ir1});
-        myData.save(function (err, data) {
-            if (err) {
-                console.log("error");
-            }
-            console.log('message is inserted');
-        });
-    }
-
-    res.status(200);
-    res.redirect(url.parse(req.url,true).query.url);
-};
-
 
